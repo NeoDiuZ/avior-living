8ce710918e5f3639c9625b7a65dc639b9f2d7677
Nyan <nhtun.23@ichat.sp.edu.sg>
2026-06-17 19:48:33 +0800

    Add use client to CartDrawer

--- src/components/site/CartDrawer.tsx
+++ src/components/site/CartDrawer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
